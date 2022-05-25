0bf4eee6af5bf610ee39832943cdaec19324d873
ClaudioFaraleski <faraleski@hotmail.com>
2022-05-25 11:14:49 -0300

    Projeto-Inicial

---
+++ src/Conta.java
@@ -0,0 +1,22 @@
+public abstract class Conta implements IMetodo{
+
+    protected  String agencia;
+    protected  int numero ;
+
+    public void sacar(double valor){
+
+    }
+    public void depositar(double valor){
+
+    }
+    public void transferir(double valor ){
+
+    }
+
+    public  void imprimirextrato(){
+
+    }
+
+
+
+}
---
+++ src/ContaCorrente.java
@@ -0,0 +1,7 @@
+public class ContaCorrente extends Conta{
+
+    @Override
+    public void imprimirextrato() {
+        super.imprimirextrato();
+    }
+}
